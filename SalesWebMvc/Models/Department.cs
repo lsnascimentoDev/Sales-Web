9bbdf7cc4afb9ce610d1dd6725e7abe60059f1b9
Lucas Nascimento <lsnascimento.dev@gmail.com>
2018-09-18 21:40:09 -0300

    Primeira Migration

---
+++ SalesWebMvc/Models/Department.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public int Nome { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+
+        }
+
+        public Department(int id, int nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+
+
+        public void AddSeller(Seller seller)
+        {
+
+            Sellers.Add(seller);
+        }
+
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+
+            return Sellers.Sum(sellers => sellers.TotalSales(initial, final));
+
+        }
+    }
+
+    
+    
+}
--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Models;
 
 namespace SalesWebMvc
 {
@@ -22,6 +24,10 @@ namespace SalesWebMvc
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<SalesWebMvcContext>(options =>
+                   options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
+                    builder.MigrationsAssembly("SalesWebMvc")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
